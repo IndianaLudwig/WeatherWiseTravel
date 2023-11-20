9d9f7380bc17922991367ced3b6c6f76943acc2f
Indiana Ludwig <83787968+IndianaLudwig@users.noreply.github.com>
2023-11-20 17:36:54 -0500

    Working with POST request

--- WWTWebApp/WWT/urls.py
+++ WWTWebApp/WWT/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
   
   # /WWT/results/
   path("results/", views.results, name="results"),
+  path("results/<str:origin>/<str:destination>/<str:arrival>/<str:departure>", views.results, name="results2"),
   
   # /WWT/location/locationID/
   path("location/<int:locationID>/", views.location, name="location"),
--- WWTWebApp/WWT/views.py
+++ WWTWebApp/WWT/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Welcome page and input form
 def index(request):
@@ -6,9 +8,24 @@ def index(request):
 
 # Results page
 def results(request):
+  try:
+      #Attempt to read in information from the request
+      origin = request.POST["origin"]
+      destination = request.POST["destination"]
+      arrival = request.POST["arrival"]
+      departure = request.POST["departure"]
+  except (KeyError):
+      #Information was missing from the request, redirect to index page
+      return render(request, 'WWT/index.html')
+  else:
+      #Information from the request was collected, direct to results page
+      context = {"origin": origin, "destination": destination, "arrival": arrival, "departure": departure}
+      return HttpResponseRedirect(reverse("WWT:results2", kwargs=context)) #need to re-add the ability to have a url with the parameters
+      return render(request, 'WWT/results.html', context)
   
-  context = {"origin": origin, "destination": destination, "arrival": arrival, "departure": departure}
-  return render(request, 'WWT/results.html', context)
+def results2(request, origin, destination, arrival, departure):
+   context = {"origin": origin, "destination": destination, "arrival": arrival, "departure": departure}
+   return render(request, 'WWT/results.html', context)
 
 # Location details page
 def location(request, locationID):
