be18aec4644b35138e776fc2a5364df662d35b43
Indiana Ludwig <83787968+IndianaLudwig@users.noreply.github.com>
2023-11-26 18:54:31 -0500

    Working on Views

--- WWTWebApp/WWT/views.py
+++ WWTWebApp/WWT/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
 
 # Welcome page and input form
 def index(request):
@@ -16,7 +14,7 @@ def results(request):
       departure = request.GET["departure"]
   except (KeyError):
       #Information was missing from the request, redirect to index page
-      return render(request, 'WWT/index.html')
+      return render(request, 'WWT/index.html', {"error_message": "Required information was missing for results."})
   else:
       #Information from the request was collected, direct to results page
       context = {"origin": origin, "destination": destination, "arrival": arrival, "departure": departure}
