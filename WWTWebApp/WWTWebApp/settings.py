d1f38feae273e1bda64843fc577108c103f2f336
Indiana Ludwig <83787968+IndianaLudwig@users.noreply.github.com>
2023-11-26 21:08:20 -0500

    Adding in CSS

--- WWTWebApp/WWTWebApp/settings.py
+++ WWTWebApp/WWTWebApp/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Finding static files in the project
+STATICFILES_DIRS = [
+    BASE_DIR/'WWT/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
