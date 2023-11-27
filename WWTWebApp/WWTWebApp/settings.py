f0e8b839aba5b8af7b4ae415c31753df30102929
Indiana Ludwig <83787968+IndianaLudwig@users.noreply.github.com>
2023-11-27 17:45:09 -0500

    CSS for welcome and about pages

--- WWTWebApp/WWTWebApp/settings.py
+++ WWTWebApp/WWTWebApp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #Finding static files in the project
 STATICFILES_DIRS = [
